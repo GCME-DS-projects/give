8d502a2a59cd63f564535a3aff9daa3a9618db2d
biniyam-29 <bnmmarkos@gmail.com>
2025-09-20 15:50:11 +0300

    is unstable, is bench mark for next work

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome @give/admin 👋
+              Welcome @give/admin my name is biny👋
             </h1>
           </div>
 
