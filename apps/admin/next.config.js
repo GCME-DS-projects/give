41e472b685b591b3bc203dd69124772b448e1fde
biniyam-29 <bnmmarkos@gmail.com>
2025-09-22 00:56:03 +0300

    set-up x-04

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -2,27 +2,26 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  // Use this to set Nx-specific options
-  // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.API_URL}/api/:path*` // Proxy to Backend
+        destination: `${process.env.API_URL}/api/:path*`
       }
     ];
   },
+  webpack(config) {
+    config.resolve.plugins = config.resolve.plugins || [];
+    config.resolve.plugins.push(new TsconfigPathsPlugin.default());
+    return config;
+  }
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
-
-module.exports = composePlugins(...plugins)(nextConfig);
+module.exports = composePlugins(withNx)(nextConfig);
--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,10 +1,12 @@
+"use client";
+import React from "react";
 import { authClient } from "../lib/auth-client";
-
+import { Alert } from "@give/shared/index";
 
 export default function Home() {
   const { isPending: isLoading } = authClient.useSession();
   if (isLoading) {
-    return <div className="flex justify-center items-center min-h-screen">Loading...</div>;
+    return <div className="flex justify-center items-center min-h-screen">Loading... <Alert>hi there!!!</Alert></div>;
   }
   return <div>Welcome to the Home page!</div>;
 }
